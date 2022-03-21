a21889ef4b2374fba8159c30c289fcc7bde901a9
James Pritchard <james.edward.pritchard@gmail.com>
2022-03-21 14:42:03 -0400

    basic blog style app using nextJS

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import Nav from './Nav'
+import Header from './Header'
+import Meta from './Meta'
+import styles from '../styles/Layout.module.css'
+
+const Layout =({children}) =>{
+    return(
+        <div>
+            <Meta/>
+            <Nav />
+            <Header/>
+            <div className={styles.container}>
+                <main className={styles.main}>
+                    {children}
+                </main>
+            </div>
+        </div>
+    )
+}
+export default Layout
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Head from 'next/Head'
+
+const about = () =>{
+    return( 
+    <div>
+        <h1>About</h1>
+    </div>
+    )
+}
+
+export default about
